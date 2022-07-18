ec577ed22eb2d39d8cbec21a2669fee44a36930b
Manuel Cerezo <cerezomazon53@gmail.com>
2022-07-18 13:32:56 +0200

    dropzone with sql

--- app.py
+++ app.py
@@ -45,20 +45,21 @@ def dropzone_upload(type):
 
     if type == 1:
         pic = request.files['file']
-        filename = secure_filename(pic.filename)
-        mimetype = pic.mimetype
-        pictures.append(Img(img=pic.read(), mimetype = mimetype, name = filename, ))
+        pictures.append(Img(img=pic.read(), mimetype = pic.mimetype, name = secure_filename(pic.filename), ))
         return "",200
 
     if type == 2:
-        add_to_sql(pictures)
-        return "",200
+        add_to_sql()
+        return {"estado":"subido"},200
     
     
 
 
-def add_to_sql(pictures):
-    print(pictures)
+def add_to_sql():
+    for pic in pictures:
+        db.session.add(pic)
+        
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
